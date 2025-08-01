1fa75da3c26b0ca74f1f05a0046b934db2d1d158
Leon Kozlowski <leonko@playtika.com>
2025-08-01 16:30:20 +0200

    super

---
+++ src/AppAndroid.java
@@ -0,0 +1,9 @@
+public class AppAndroid extends App{
+    public AppAndroid(String name) {
+        super(name);
+        appInfo();
+    }
+    public void runAndroidApp(){
+        System.out.println("Andoid app " + name + " is running");
+    }
+}
---
+++ src/AppIphone.java
@@ -0,0 +1,10 @@
+public class AppIphone extends App{
+
+    public AppIphone(String name) {
+        super(name);
+        appInfo();
+    }
+    public void runIphoneApp(){
+        System.out.println("Iphone app " + name + " is running");
+    }
+}
